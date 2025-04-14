03a554d6bc1773308051d6dad3c3c1cb76f601ba
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-04-14 23:45:27 +0600

    first test tried with class selector

--- src/test/java/DigitaluniteTest.java
+++ src/test/java/DigitaluniteTest.java
@@ -1,11 +1,13 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
+import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Check the form submitted successfully")
 public class DigitaluniteTest {
 
     private static WebDriver driver;
@@ -16,9 +18,16 @@ public class DigitaluniteTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
-@Test
-    public void FormSubmit(){
+
+    @Test
+    public void FormSubmit() throws InterruptedException {
         driver.get("https://www.digitalunite.com/practice-webform-learners");
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        Thread.sleep(200);
+        List<WebElement> formFields= driver.findElements(By.className(".form-control"));
+        formFields.get(0).sendKeys("Test Name");
+        formFields.get(1).sendKeys("1234567890");
+
 
     }
 
