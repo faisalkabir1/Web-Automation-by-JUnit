bec9b5ecbcbb371307e2d4cec83875d68eccd850
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-04-17 21:29:13 +0600

    Enhance form submission flow with scroll and final checks
    Added scrolling functionality using JavascriptExecutor for better form navigation.

--- src/test/java/WpeverestTest.java
+++ src/test/java/WpeverestTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,15 +30,10 @@ public class WpeverestTest {
         elements.get(7).sendKeys("Name"); //LastName
         elements.get(5).sendKeys("name@gmail.com"); //email
         elements.get(8).click(); //gender
-
         elements.get(6).sendKeys("Abcd@$1234");  //password
-
         elements.get(11).click(); //DateofBirth
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
-      // List<WebElement> monthDropdown = driver.findElements(By.tagName("select"));
-      //  monthDropdown.get(2).click();
-      //  Thread.sleep(4000);
         //Select Month
         List<WebElement> selectMonth = driver.findElements(By.tagName("option"));
         selectMonth.get(254).click();
@@ -54,10 +50,16 @@ public class WpeverestTest {
         elements.get(16).sendKeys("Bangladeshi");//Nationality
         elements.get(14).sendKeys("1916603009");//Phone no
 
-
         Select country = new Select(driver.findElement(By.id("country_1665629257")));//Country
         country.selectByVisibleText("Bangladesh");
-
+        //Scroll
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+        //Check on Terms & Conditions
+        elements.get(33).click();
+        //Click on submit button
+        WebElement submitButton = driver.findElement(By.cssSelector("button.ur-submit-button"));
+        submitButton.click();
 
     }
 
