440d6f637b879efd44c1e1fb55406dc8e267f1bb
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-04-18 23:20:47 +0600

    Add table parsing logic to DataScrapingTest
    Updated the test to locate a table element, iterate through its rows and cells, and print cell contents.

--- src/test/java/WebScrapingTest.java
+++ src/test/java/WebScrapingTest.java
@@ -1,10 +1,13 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.List;
 
 public class WebScrapingTest {
     private static WebDriver driver;
@@ -21,8 +24,18 @@ public class WebScrapingTest {
         driver.quit();
     }
     @Test
-    public void DataScrapingTest(){
+    public void DataScrapingTest() {
         driver.get("https://dsebd.org/latest_share_price_scroll_by_value.php");
+        WebElement table = driver.findElement(By.className("table-responsive"));
+        List<WebElement> allRows = table.findElements(By.tagName("tr"));
+        int i = 0;
+        for (WebElement row : allRows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            for (WebElement cell : cells) {
+                i++;
+                System.out.println("num[" + i + "] " + cell.getText());
 
+            }
+        }
     }
 }
